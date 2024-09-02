2a214c0d35bb9483340e881bd03afceddc2067c9
sudeepmi <sudeepm0599@gmail.com>
2024-09-02 12:03:40 +0545

    migrations and models created

---
+++ app/Models/Music.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Music extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['artist_id', 'title', 'album_name', 'genre'];
+
+    public function artist(){
+        return $this->belongsTo(Artist::class);
+    }
+}
---
+++ database/migrations/2024_09_02_060322_create_musics_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('musics', function (Blueprint $table) {
+            $table->id();
+            $table->biginteger('artist_id')->unsigned();
+            $table->foreign('artist_id', 'musics_artist_id_foreign')->references('id')->on('artists')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('musics');
+    }
+};
